3767c8165cdbd6caa2d0e310002b83363e89d73c
Coffeinum00 <coffeinum00@gmail.com>
2022-03-06 14:29:29 +0100

    five

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/screens/auth/data/providers/auth_state.dart';
+import 'package:flutter_firebase/screens/home/widgets/car_modal.dart';
 import 'package:provider/provider.dart';
 
+import 'data/models/cars_model.dart';
+
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  List<String> documentsID = [];
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +17,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         actions: [
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
             onPressed: () {
               Provider.of<AuthState>(context, listen: false).signOut();
             },
@@ -20,31 +26,77 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: ListView.builder(
-              itemCount: 5,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return listItem('Brand', index.toString());
+          padding: const EdgeInsets.all(20.0),
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance.collection('cars').snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  List<Cars> _listOfCars =
+                      snapshot.data!.docs.map((DocumentSnapshot e) {
+                    Map<String, dynamic> data =
+                        e.data()! as Map<String, dynamic>;
+
+                    return Cars.fromJson(data);
+                  }).toList();
+
+                  documentsID =
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    return document.id;
+                  }).toList();
+
+                  return ListView.builder(
+                      itemCount: _listOfCars.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        Cars _car = _listOfCars[index];
+                        return listItem(_car, documentsID[index], context);
+                      });
+                }
               }),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Cars data = Cars(model: 'Camry', brand: 'Toyota');
+          FirebaseFirestore.instance.collection('cars').add(data.toJson());
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
 
-  Widget listItem(brand, model) {
+  Widget listItem(Cars car, documentID, context) {
     return Dismissible(
       key: UniqueKey(),
       onDismissed: (direction) {
         if (direction == DismissDirection.startToEnd ||
-            direction == DismissDirection.endToStart) print('Delete');
+            direction == DismissDirection.endToStart) {
+          FirebaseFirestore.instance
+              .collection('cars')
+              .doc(documentID)
+              .delete();
+        }
       },
       child: ListTile(
-        title: Text(brand),
-        subtitle: Text(model),
+        title: Text(car.brand!),
+        subtitle: Text(car.model!),
         trailing: IconButton(
-          icon: Icon(Icons.edit),
-          onPressed: () {},
+          icon: const Icon(Icons.edit),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) => Dialog(
+                child: CarModal(
+                  car: car,
+                  docID: documentID,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
---
+++ lib/screens/home/widgets/car_modal.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home/data/models/cars_model.dart';
+
+class CarModal extends StatefulWidget {
+  CarModal({Key? key, this.car, this.docID}) : super(key: key);
+
+  final Cars? car;
+  final String? docID;
+
+  @override
+  State<CarModal> createState() => _CarModalState();
+}
+
+class _CarModalState extends State<CarModal> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController? _brandController;
+  TextEditingController? _modelController;
+  Cars? _car;
+
+  @override
+  void initState() {
+    _car = widget.car!;
+    _brandController = TextEditingController(text: _car!.brand);
+    _modelController = TextEditingController(text: _car!.model);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                onChanged: (v) {
+                  setState(() {
+                    _car!.brand = v;
+                  });
+                },
+                controller: _brandController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter brand';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                onChanged: (v) {
+                  setState(() {
+                    _car!.model = v;
+                  });
+                },
+                controller: _modelController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter model';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancel')),
+                TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        FirebaseFirestore.instance
+                            .collection('cars')
+                            .doc(widget.docID)
+                            .update(_car!.toJson())
+                            .whenComplete(() => Navigator.of(context).pop());
+                      }
+                    },
+                    child: const Text('Save')),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
