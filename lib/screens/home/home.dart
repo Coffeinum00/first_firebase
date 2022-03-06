afed9e2ef2f66211976567bef890ec1af8e8e216
Coffeinum00 <coffeinum00@gmail.com>
2022-03-06 11:40:45 +0100

    five

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,13 +8,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Provider.of<AuthState>(context, listen: false).signOut();
+            },
+          ),
+        ],
+      ),
       body: Center(
-        child: TextButton(
-          child: Text('Log out'),
-          onPressed: () {
-            Provider.of<AuthState>(context, listen: false).signOut();
-          },
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: ListView.builder(
+              itemCount: 5,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return listItem('Brand', index.toString());
+              }),
+        ),
+      ),
+    );
+  }
+
+  Widget listItem(brand, model) {
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        if (direction == DismissDirection.startToEnd ||
+            direction == DismissDirection.endToStart) print('Delete');
+      },
+      child: ListTile(
+        title: Text(brand),
+        subtitle: Text(model),
+        trailing: IconButton(
+          icon: Icon(Icons.edit),
+          onPressed: () {},
         ),
       ),
     );
